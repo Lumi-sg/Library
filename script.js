0da43b2f74ed1895f663d5d7dd7c2110dee009de
Lumi <shaicloud@gmail.com>
2022-11-23 17:40:42 -0500

    modal functional

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-let myLibrary = [];
+const addBookButton = document.querySelector("#addBook");
+const modal = document.querySelector(".modal");
+const modalSpan = document.getElementsByClassName("close")[0];
+const bookTitle = document.getElementById(".title");
+const bookAuthor = document.getElementById(".author");
+const bookRating = document.getElementById(".rating");
+const bookYearPublished = document.getElementById(".yearPublished");
+const bookHasRead = document.getElementById(".hasRead");
 
-class Book {
-	constructor(title, author, status, rating) {
-		this.title = title;
-		this.author = author;
-		this.status = status;
-		this.rating = rating;
-	}
+addBookButton.addEventListener("click", () => {
+	openBookModal();
+});
+
+modalSpan.addEventListener("click", () => {
+	closeBookModal();
+});
+
+function openBookModal() {
+	modal.style.display = "block";
 }
 
-function AddBook() {
-	let title = prompt("What is the title of the book");
-	let author = prompt("What is the author of the book");
-	let status = prompt("Have you read the book?");
-	let rating = prompt("What is your rating of the book /5?");
-
-	const newBook = new Book(title, author, status, rating);
-	myLibrary.push(newBook);
+function closeBookModal() {
+	modal.style.display = "none";
 }
 
-AddBook();
+//date validation
+let yearPublished = document.getElementById("yearPublished");
+yearPublished.max = new Date().toLocaleDateString("en-ca");
+
+// let myLibrary = [];
+
+// class Book {
+// 	constructor(title, author, status, rating) {
+// 		this.title = title;
+// 		this.author = author;
+// 		this.status = status;
+// 		this.rating = rating;
+// 	}
+// }
+
+// function AddBook() {
+// 	let title = prompt("What is the title of the book");
+// 	let author = prompt("What is the author of the book");
+// 	let status = prompt("Have you read the book?");
+// 	let rating = prompt("What is your rating of the book /5?");
+
+// 	const newBook = new Book(title, author, status, rating);
+// 	myLibrary.push(newBook);
+// }
+
+// AddBook();
